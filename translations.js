d109d4110f1c98cd8784812106a51e450f4e3c57
João Santos <dev.joaopedrovieira@gmail.com>
2024-06-20 15:19:09 +0100

    first commit

---
+++ translations.js
@@ -0,0 +1,254 @@
+var linguJSON = {
+  languages: [
+    {
+      lang_name: "PT",
+      lang_code: "pt",
+      url_pattern: "?",
+    },
+    {
+      lang_name: "EN",
+      lang_code: "en",
+      url_pattern: "?",
+    },
+    {
+      lang_name: "FR",
+      lang_code: "fr",
+      url_pattern: "?",
+    },
+    {
+      lang_name: "ES",
+      lang_code: "es",
+      url_pattern: "?",
+    },
+  ],
+  custom_lang_switcher_html:
+    '<div id="langswitcher" class="notranslate"><a id="currlanglink" class="language-item" href="">[[linguCurrLang]]</a><div class="divider-languages"></div><a href="[[linguTargetLangHref1]]" class="targetlang language-item">[[linguTargetLang1]]</a><div class="divider-languages"></div><a href="[[linguTargetLangHref2]]" class="targetlang language-item">[[linguTargetLang2]]</a><div class="divider-languages"></div><a href="[[linguTargetLangHref3]]" class="targetlang language-item">[[linguTargetLang3]]</a></div>',
+  custom_lang_switcher_container_id: "langlinks",
+
+  translated_segments: [
+    {
+      source: "Institucional",
+      target_en: "Institutional",
+      target_fr: "Institutionnel",
+      target_es: "Institucional",
+      target_de: "Institutionell",
+    },
+    {
+      source: "Serviços",
+      target_en: "Services",
+      target_fr: "Services",
+      target_es: "Servicios",
+      target_de: "Dienstleistungen",
+    },
+    {
+      source: "Design, Protótipos e Pequenas Séries",
+      target_en: "Design, Prototypes and Small Series",
+      target_fr: "Design, Prototypes et Petites Séries",
+      target_es: "Diseño, Prototipos y Pequeñas Series",
+      target_de: "Design, Prototypen und Kleinserien",
+    },
+    {
+      source: "Produção de Moldes e Validação",
+      target_en: "Mold Production and Validation",
+      target_fr: "Production de Moules et Validation",
+      target_es: "Producción de Moldes y Validación",
+      target_de: "Formenbau und Validierung",
+    },
+    {
+      source: "Injeção de Termoplásticos e Assemblagem",
+      target_en: "Thermoplastic Injection and Assembly",
+      target_fr: "Injection de Thermoplastiques et Assemblage",
+      target_es: "Inyección de Termoplásticos y Montaje",
+      target_de: "Thermoplast-Spritzguss und Montage",
+    },
+    {
+      source: "Industrias | Casos de Estudo",
+      target_en: "Industries | Case Studies",
+      target_fr: "Industries | Études de Cas",
+      target_es: "Industrias | Casos de Estudio",
+      target_de: "Branchen | Fallstudien",
+    },
+    {
+      source: "Notícias",
+      target_en: "News",
+      target_fr: "Actualités",
+      target_es: "Noticias",
+      target_de: "Nachrichten",
+    },
+    {
+      source: "Contactos",
+      target_en: "Contacts",
+      target_fr: "Contacts",
+      target_es: "Contactos",
+      target_de: "Kontakte",
+    },
+    {
+      source: "home",
+      target_en: "home",
+      target_fr: "accueil",
+      target_es: "inicio",
+      target_de: "startseite",
+    },
+    {
+      source:
+        "Fundada em 1993, a <strong>TCC</strong> é uma empresa de produção de moldes de precisão em aço especializada na <strong>ELABORAÇÃO DE PROJETOS</strong>, <strong>FABRICO</strong> e <strong>CONTROLO DE QUALIDADE</strong> de moldes de injeção para a indústria de plásticos.",
+      target_en:
+        "Founded in 1993, <strong>TCC</strong> is a company specializing in the production of precision steel molds, specializing in <strong>PROJECT DEVELOPMENT</strong>, <strong>MANUFACTURING</strong>, and <strong>QUALITY CONTROL</strong> of injection molds for the plastics industry.",
+      target_fr:
+        "Fondée en 1993, <strong>TCC</strong> est une entreprise spécialisée dans la production de moules de précision en acier, spécialisée dans l'<strong>ÉLABORATION DE PROJETS</strong>, la <strong>FABRICATION</strong> et le <strong>CONTRÔLE DE QUALITÉ</strong> de moules d'injection pour l'industrie du plastique.",
+      target_es:
+        "Fundada en 1993, <strong>TCC</strong> es una empresa especializada en la producción de moldes de precisión en acero, especializada en la <strong>ELABORACIÓN DE PROYECTOS</strong>, <strong>FABRICACIÓN</strong> y <strong>CONTROL DE CALIDAD</strong> de moldes de inyección para la industria del plástico.",
+      target_de:
+        "Gegründet im Jahr 1993, <strong>TCC</strong> ist ein Unternehmen, das sich auf die Herstellung von Präzisionsstahlformen spezialisiert hat, spezialisiert auf die <strong>PROJEKTENTWICKLUNG</strong>, <strong>HERSTELLUNG</strong> und <strong>QUALITÄTSKONTROLLE</strong> von Spritzgussformen für die Kunststoffindustrie.",
+    },
+    {
+      source:
+        "Situada na Marinha Grande, um dos maiores centros industriais de moldes da Europa, a <strong>TCC</strong> orgulha-se de ter como referência clientes exigentes localizados em países tais como a Espanha, França, Bélgica, Holanda, Finlândia, Suécia, Dinamarca, Brasil e México.",
+      target_en:
+        "Located in Marinha Grande, one of the largest industrial mold centers in Europe, <strong>TCC</strong> prides itself on serving demanding clients located in countries such as Spain, France, Belgium, Netherlands, Finland, Sweden, Denmark, Brazil, and Mexico.",
+      target_fr:
+        "Située à Marinha Grande, l'un des plus grands centres industriels de moules en Europe, <strong>TCC</strong> est fière de compter parmi ses clients exigeants des pays tels que l'Espagne, la France, la Belgique, les Pays-Bas, la Finlande, la Suède, le Danemark, le Brésil et le Mexique.",
+      target_es:
+        "Ubicada en Marinha Grande, uno de los mayores centros industriales de moldes en Europa, <strong>TCC</strong> se enorgullece de contar con clientes exigentes ubicados en países como España, Francia, Bélgica, Países Bajos, Finlandia, Suecia, Dinamarca, Brasil y México.",
+      target_de:
+        "Gelegen in Marinha Grande, einem der größten industriellen Formenzentren Europas, ist <strong>TCC</strong> stolz darauf, anspruchsvolle Kunden in Ländern wie Spanien, Frankreich, Belgien, Niederlande, Finnland, Schweden, Dänemark, Brasilien und Mexiko zu haben.",
+    },
+    {
+      source:
+        "Graças à nossa experiência e amplo conhecimento da indústria dos moldes conjugado com a utilização de tecnologia de ponta, podemos fornecer moldes de alta qualidade, independentemente da sua complexidade, a um preço muito competitivo.",
+      target_en:
+        "Thanks to our experience and extensive knowledge of the mold industry combined with the use of cutting-edge technology, we can provide high-quality molds, regardless of their complexity, at a very competitive price.",
+      target_fr:
+        "Grâce à notre expérience et à notre connaissance approfondie de l'industrie des moules associée à l'utilisation de technologies de pointe, nous pouvons fournir des moules de haute qualité, quelle que soit leur complexité, à un prix très compétitif.",
+      target_es:
+        "Gracias a nuestra experiencia y amplio conocimiento de la industria de moldes combinado con el uso de tecnología de vanguardia, podemos proporcionar moldes de alta calidad, independientemente de su complejidad, a un precio muy competitivo.",
+      target_de:
+        "Dank unserer Erfahrung und umfangreichen Kenntnisse in der Formenindustrie in Verbindung mit dem Einsatz modernster Technologie können wir hochwertige Formen unabhängig von ihrer Komplexität zu einem sehr wettbewerbsfähigen Preis anbieten.",
+    },
+    {
+      source:
+        "Conscientes da agressividade do mercado e das necessidades dos nossos clientes, procuramos as melhores e mais otimizadas soluções tendo em vista a obtenção de prazos cada vez mais curtos.",
+      target_en:
+        "Aware of the market's aggressiveness and our clients' needs, we seek the best and most optimized solutions aiming at achieving increasingly shorter deadlines.",
+      target_fr:
+        "Conscients de l'agressivité du marché et des besoins de nos clients, nous recherchons les meilleures solutions optimisées visant à atteindre des délais de plus en plus courts.",
+      target_es:
+        "Conscientes de la agresividad del mercado y de las necesidades de nuestros clientes, buscamos las mejores soluciones optimizadas con el objetivo de lograr plazos cada vez más cortos.",
+      target_de:
+        "Bewusst über die Aggressivität des Marktes und die Bedürfnisse unserer Kunden suchen wir die besten und optimierten Lösungen, um immer kürzere Fristen zu erreichen.",
+    },
+    {
+      source:
+        "Na TCC, cuidamos do seu projeto desde a conceção até à produção final. Os nossos engenheiros trabalham para lhe oferecer rapidamente um protótipo e a primeira produção das suas peças.",
+      target_en:
+        "At TCC, we take care of your project from conception to final production. Our engineers work to quickly provide you with a prototype and the initial production of your parts.",
+      target_fr:
+        "Chez TCC, nous nous occupons de votre projet de la conception à la production finale. Nos ingénieurs travaillent pour vous fournir rapidement un prototype et la première production de vos pièces.",
+      target_es:
+        "En TCC, nos ocupamos de su proyecto desde la concepción hasta la producción final. Nuestros ingenieros trabajan para ofrecerle rápidamente un prototipo y la primera producción de sus piezas.",
+      target_de:
+        "Bei TCC kümmern wir uns um Ihr Projekt von der Konzeption bis zur endgültigen Produktion. Unsere Ingenieure arbeiten daran, Ihnen schnell einen Prototypen und die erste Produktion Ihrer Teile zur Verfügung zu stellen.",
+    },
+    {
+      source: "Saber mais",
+      target_en: "Learn more",
+      target_fr: "En savoir plus",
+      target_es: "Más información",
+      target_de: "Mehr erfahren",
+    },
+    {
+      source:
+        "Trabalhamos em estreita colaboração com centros de testes que nos permitem ter uma grande eficiência e reduzir o tempo antes de enviar os moldes.",
+      target_en:
+        "We work closely with testing centers that allow us to achieve high efficiency and reduce lead time before sending out the molds.",
+      target_fr:
+        "Nous travaillons en étroite collaboration avec des centres de tests qui nous permettent d'atteindre une grande efficacité et de réduire le délai avant l'envoi des moules.",
+      target_es:
+        "Trabajamos en estrecha colaboración con centros de pruebas que nos permiten lograr una gran eficiencia y reducir el tiempo antes de enviar los moldes.",
+      target_de:
+        "Wir arbeiten eng mit Testzentren zusammen, die es uns ermöglichen, hohe Effizienz zu erzielen und die Vorlaufzeit vor dem Versand der Formen zu reduzieren.",
+    },
+    {
+      source:
+        "Dotados de certificação ISO 6 e de uma sala limpa, podemos testar moldes destinados à indústria farmacêutica ou médica. Dispomos ainda de duas fresadoras CNC novas, equipadas com software de medição (protocolo de medição BLUM) que permite inspecionar qualquer peça, permitindo ganhos significativos de produtividade e fiabilidade.",
+      target_en:
+        "Equipped with ISO 6 certification and a clean room, we can test molds intended for the pharmaceutical or medical industry. We also have two new CNC milling machines equipped with measurement software (BLUM measurement protocol) that allows inspection of any part, resulting in significant gains in productivity and reliability.",
+      target_fr:
+        "Dotés de la certification ISO 6 et d'une salle blanche, nous pouvons tester des moules destinés à l'industrie pharmaceutique ou médicale. Nous disposons également de deux nouvelles fraiseuses CNC équipées d'un logiciel de mesure (protocole de mesure BLUM) qui permet l'inspection de toute pièce, entraînant des gains significatifs de productivité et de fiabilité.",
+      target_es:
+        "Dotados de certificación ISO 6 y de una sala limpia, podemos probar moldes destinados a la industria farmacéutica o médica. También contamos con dos fresadoras CNC nuevas equipadas con software de medición (protocolo de medición BLUM) que permite inspeccionar cualquier pieza, lo que proporciona ganancias significativas en productividad y fiabilidad.",
+      target_de:
+        "Ausgestattet mit ISO 6 Zertifizierung und einem Reinraum können wir Formen für die pharmazeutische oder medizinische Industrie testen. Wir verfügen außerdem über zwei neue CNC-Fräsmaschinen, ausgestattet mit Messsoftware (BLUM Messprotokoll), die die Inspektion jedes Teils ermöglicht und so signifikante Produktivitäts- und Zuverlässigkeitsgewinne bringt.",
+    },
+    {
+      source: "Aeronáutica",
+      target_en: "Aeronautics",
+      target_fr: "Aéronautique",
+      target_es: "Aeronáutica",
+      target_de: "Luftfahrt",
+    },
+    {
+      source: "Área Automóvel",
+      target_en: "Automotive Sector",
+      target_fr: "Domaine Automobile",
+      target_es: "Sector Automotriz",
+      target_de: "Automobilbereich",
+    },
+    {
+      source: "Área Médica",
+      target_en: "Medical Sector",
+      target_fr: "Domaine Médical",
+      target_es: "Sector Médico",
+      target_de: "Medizinbereich",
+    },
+    {
+      source: "Eletro-eletrónica",
+      target_en: "Electroelectronics",
+      target_fr: "Électroélectronique",
+      target_es: "Electroelectrónica",
+      target_de: "Elektroelektronik",
+    },
+    {
+      source: "Área Alimentar",
+      target_en: "Food Sector",
+      target_fr: "Domaine Alimentaire",
+      target_es: "Sector Alimentario",
+      target_de: "Lebensmittelbereich",
+    },
+    {
+      source: "Houseware",
+      target_en: "Houseware",
+      target_fr: "Articles ménagers",
+      target_es: "Artículos para el hogar",
+      target_de: "Haushaltswaren",
+    },
+    {
+      source: "Agricultura e Indústria",
+      target_en: "Agriculture and Industry",
+      target_fr: "Agriculture et Industrie",
+      target_es: "Agricultura e Industria",
+      target_de: "Landwirtschaft und Industrie",
+    },
+    {
+      source: "Packaging",
+      target_en: "Packaging",
+      target_fr: "Emballage",
+      target_es: "Embalaje",
+      target_de: "Verpackung",
+    },
+    {
+      source: "Indústrias que servimos",
+      target_en: "Industries we serve",
+      target_fr: "Industries que nous servons",
+      target_es: "Industrias que servimos",
+      target_de: "Branchen, die wir bedienen",
+    },
+    {
+      source: "Feedback de clientes",
+      target_en: "Customer feedback",
+      target_fr: "Feedback des clients",
+      target_es: "Opiniones de clientes",
+      target_de: "Kundenfeedback",
+    },
+  ],
+};
