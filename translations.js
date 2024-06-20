8078103c69ab2838d6146b419fce51c67dd8745b
João Santos <dev.joaopedrovieira@gmail.com>
2024-06-20 15:54:40 +0100

    institucional

--- translations.js
+++ translations.js
@@ -89,6 +89,9 @@ var linguJSON = {
       target_es: "inicio",
       target_de: "startseite",
     },
+   
+    //HOME
+
     {
       source:
         "Fundada em 1993, a <strong>TCC</strong> é uma empresa de produção de moldes de precisão em aço especializada na <strong>ELABORAÇÃO DE PROJETOS</strong>, <strong>FABRICO</strong> e <strong>CONTROLO DE QUALIDADE</strong> de moldes de injeção para a indústria de plásticos.",
@@ -250,5 +253,148 @@ var linguJSON = {
       target_es: "Opiniones de clientes",
       target_de: "Kundenfeedback",
     },
+
+    //INSTITUCIONAL
+    {
+      "source": "Na vanguarda da precisão, a moldar o futuro da indústria com excelência.",
+      "target_en": "At the forefront of precision, shaping the future of the industry with excellence.",
+      "target_fr": "À la pointe de la précision, façonnant l'avenir de l'industrie avec excellence.",
+      "target_es": "A la vanguardia de la precisión, moldeando el futuro de la industria con excelencia.",
+      "target_de": "An der Spitze der Präzision, formt die Zukunft der Industrie mit Exzellenz."
+    },
+    {
+      "source": "Na TCC encontra uma equipa técnica composta por projetistas experientes, que apoiados por sistemas CAD/CAM desenvolvem os projetos dos moldes, sendo também responsáveis pelo acompanhamento de todas a etapas de fabricação.",
+      "target_en": "At TCC, you will find a technical team composed of experienced designers who, supported by CAD/CAM systems, develop mold projects and are also responsible for monitoring all stages of manufacturing.",
+      "target_fr": "Chez TCC, vous trouverez une équipe technique composée de concepteurs expérimentés qui, soutenus par des systèmes CAD/CAM, développent les projets de moules et sont également responsables du suivi de toutes les étapes de fabrication.",
+      "target_es": "En TCC encontrará un equipo técnico compuesto por diseñadores experimentados que, apoyados por sistemas CAD/CAM, desarrollan proyectos de moldes y también son responsables del seguimiento de todas las etapas de fabricación.",
+      "target_de": "Bei TCC finden Sie ein technisches Team erfahrener Designer, die unterstützt von CAD/CAM-Systemen Formenprojekte entwickeln und auch für die Überwachung aller Fertigungsstufen verantwortlich sind."
+    },
+    {
+      "source": "A nossa excelente organização permite-nos gerir projetos de grande envergadura sem descurar a qualidade, respeitando imperativamente os prazos de entrega.",
+      "target_en": "Our excellent organization allows us to manage large-scale projects without neglecting quality, strictly respecting delivery deadlines.",
+      "target_fr": "Notre excellente organisation nous permet de gérer des projets de grande envergure sans négliger la qualité, en respectant strictement les délais de livraison.",
+      "target_es": "Nuestra excelente organización nos permite gestionar proyectos de gran envergadura sin descuidar la calidad, respetando estrictamente los plazos de entrega.",
+      "target_de": "Durch unsere ausgezeichnete Organisation können wir Großprojekte verwalten, ohne die Qualität zu vernachlässigen, und dabei die Lieferfristen strikt einhalten."
+    },
+    {
+      "source": "Os nossos comerciais mantêm um contacto estreito com os nossos clientes desde o primeiro contacto, garantindo um acompanhamento eficiente de todo o processo de fabrico do molde e fornecendo um serviço de pós-venda de qualidade.",
+      "target_en": "Our sales representatives maintain close contact with our clients from the first interaction, ensuring efficient monitoring of the entire mold manufacturing process and providing quality after-sales service.",
+      "target_fr": "Nos commerciaux maintiennent un contact étroit avec nos clients dès le premier contact, assurant un suivi efficace de tout le processus de fabrication du moule et fournissant un service après-vente de qualité.",
+      "target_es": "Nuestros comerciales mantienen un contacto estrecho con nuestros clientes desde el primer contacto, garantizando un seguimiento eficiente de todo el proceso de fabricación del molde y proporcionando un servicio postventa de calidad.",
+      "target_de": "Unsere Vertriebsmitarbeiter pflegen engen Kontakt zu unseren Kunden von der ersten Kontaktaufnahme an, um eine effiziente Überwachung des gesamten Formherstellungsprozesses zu gewährleisten und einen qualitativ hochwertigen After-Sales-Service zu bieten."
+    },
+    {
+      "source": "Abraçar desafios com inovação, precisão e espírito de equipa:",
+      "target_en": "Embracing challenges with innovation, precision, and teamwork:",
+      "target_fr": "Relever les défis avec innovation, précision et esprit d'équipe :",
+      "target_es": "Afrontar desafíos con innovación, precisión y espíritu de equipo:",
+      "target_de": "Herausforderungen mit Innovation, Präzision und Teamgeist angehen:"
+    },
+    {
+      "source": "a alma da nossa cultura, a força do nosso mindset.",
+      "target_en": "the soul of our culture, the strength of our mindset.",
+      "target_fr": "l'âme de notre culture, la force de notre état d'esprit.",
+      "target_es": "el alma de nuestra cultura, la fuerza de nuestra mentalidad.",
+      "target_de": "die Seele unserer Kultur, die Stärke unserer Denkweise."
+    },
+    {
+      "source": "A TCC destaca-se pela sua abordagem inovadora e orientada para a excelência, destacando-se no mercado pela precisão dos seus produtos e pelo forte espírito de equipa que impulsiona cada projeto.",
+      "target_en": "TCC stands out for its innovative and excellence-oriented approach, distinguishing itself in the market for the precision of its products and the strong teamwork spirit that drives each project.",
+      "target_fr": "TCC se distingue par son approche innovante et axée sur l'excellence, se distinguant sur le marché par la précision de ses produits et l'esprit d'équipe fort qui anime chaque projet.",
+      "target_es": "TCC se destaca por su enfoque innovador y orientado a la excelencia, destacándose en el mercado por la precisión de sus productos y por el fuerte espíritu de equipo que impulsa cada proyecto.",
+      "target_de": "TCC zeichnet sich durch ihren innovativen und exzellenzorientierten Ansatz aus, der sich im Markt durch die Präzision ihrer Produkte und den starken Teamgeist auszeichnet, der jedes Projekt antreibt."
+    },
+    {
+      "source": "Com um compromisso inabalável com a qualidade e um foco contínuo na satisfação do cliente, a TCC é a escolha ideal para aqueles que procuram soluções de moldagem de alto nível, onde a tradição de excelência encontra-se com a vanguarda da tecnologia.",
+      "target_en": "With an unwavering commitment to quality and a continuous focus on customer satisfaction, TCC is the ideal choice for those seeking high-level molding solutions, where the tradition of excellence meets cutting-edge technology.",
+      "target_fr": "Avec un engagement inébranlable envers la qualité et une attention continue à la satisfaction du client, TCC est le choix idéal pour ceux qui recherchent des solutions de moulage de haut niveau, où la tradition d'excellence rencontre la technologie de pointe.",
+      "target_es": "Con un compromiso inquebrantable con la calidad y un enfoque continuo en la satisfacción del cliente, TCC es la opción ideal para aquellos que buscan soluciones de moldeo de alto nivel, donde la tradición de excelencia se encuentra con la vanguardia de la tecnología.",
+      "target_de": "Mit einem unerschütterlichen Qualitätsanspruch und einem kontinuierlichen Fokus auf Kundenzufriedenheit ist TCC die ideale Wahl für diejenigen, die hochwertige Formlösungen suchen, wo Tradition auf Exzellenz trifft und Spitzentechnologie."
+    },
+    {
+      "source": "Inovação e Tecnologia",
+      "target_en": "Innovation and Technology",
+      "target_fr": "Innovation et Technologie",
+      "target_es": "Innovación y Tecnología",
+      "target_de": "Innovation und Technologie"
+    },
+    {
+      "source": "A TCC valoriza a inovação e a adoção de tecnologias avançadas para melhorar a qualidade e eficiência dos produtos.",
+      "target_en": "TCC values innovation and the adoption of advanced technologies to improve the quality and efficiency of products.",
+      "target_fr": "TCC valorise l'innovation et l'adoption de technologies avancées pour améliorer la qualité et l'efficacité des produits.",
+      "target_es": "TCC valora la innovación y la adopción de tecnologías avanzadas para mejorar la calidad y eficiencia de los productos.",
+      "target_de": "TCC schätzt Innovation und die Nutzung fortschrittlicher Technologien zur Verbesserung der Qualität und Effizienz von Produkten."
+    },
+    {
+      "source": "Trabalho de equipa e Colaboração",
+      "target_en": "Teamwork and Collaboration",
+      "target_fr": "Travail d'équipe et Collaboration",
+      "target_es": "Trabajo en equipo y Colaboración",
+      "target_de": "Teamarbeit und Zusammenarbeit"
+    },
+    {
+      "source": "Devido à natureza complexa dos projetos de fabricação de moldes, a TCC valoriza o trabalho em equipa e a colaboração entre diferentes departamentos e especialidades.",
+      "target_en": "Due to the complex nature of mold manufacturing projects, TCC values teamwork and collaboration among different departments and specialties.",
+      "target_fr": "En raison de la nature complexe des projets de fabrication de moules, TCC valorise le travail d'équipe et la collaboration entre différents départements et spécialités.",
+      "target_es": "Debido a la naturaleza compleja de los proyectos de fabricación de moldes, TCC valora el trabajo en equipo y la colaboración entre diferentes departamentos y especialidades.",
+      "target_de": "Aufgrund der komplexen Natur von Formenbau-Projekten schätzt TCC Teamarbeit und Zusammenarbeit zwischen verschiedenen Abteilungen und Fachrichtungen."
+    },
+    {
+      "source": "Compromisso com o cliente",
+      "target_en": "Commitment to Customer",
+      "target_fr": "Engagement envers le client",
+      "target_es": "Compromiso con el cliente",
+      "target_de": "Kundenzufriedenheit"
+    },
+    {
+      "source": "A satisfação do cliente é essencial para nós. Na TCC procuramos entender e atender às necessidades específicas dos clientes de forma eficaz.",
+      "target_en": "Customer satisfaction is essential to us. At TCC, we strive to understand and meet the specific needs of our customers effectively.",
+      "target_fr": "La satisfaction du client est essentielle pour nous. À la TCC, nous nous efforçons de comprendre et de répondre efficacement aux besoins spécifiques de nos clients.",
+      "target_es": "La satisfacción del cliente es esencial para nosotros. En TCC, buscamos entender y satisfacer las necesidades específicas de nuestros clientes de manera efectiva.",
+      "target_de": "Kundenzufriedenheit steht bei uns an erster Stelle. Bei TCC bemühen wir uns, die spezifischen Bedürfnisse unserer Kunden effektiv zu verstehen und zu erfüllen."
+    },
+    {
+      "source": "Precisão e Qualidade",
+      "target_en": "Precision and Quality",
+      "target_fr": "Précision et Qualité",
+      "target_es": "Precisión y Calidad",
+      "target_de": "Präzision und Qualität"
+    },
+    {
+      "source": "A fabricação de moldes requer altos padrões de precisão e qualidade. Portanto, a cultura da TCC enfatiza o compromisso com a excelência e a busca pela perfeição em cada etapa do processo de produção.",
+      "target_en": "Mold manufacturing requires high standards of precision and quality. Therefore, TCC's culture emphasizes a commitment to excellence and the pursuit of perfection at every stage of the production process.",
+      "target_fr": "La fabrication de moules exige des normes élevées de précision et de qualité. Par conséquent, la culture de TCC met l'accent sur l'engagement envers l'excellence et la recherche de la perfection à chaque étape du processus de production.",
+      "target_es": "La fabricación de moldes requiere altos estándares de precisión y calidad. Por lo tanto, la cultura de TCC enfatiza el compromiso con la excelencia y la búsqueda de la perfección en cada etapa del proceso de producción.",
+      "target_de": "Die Herstellung von Formen erfordert hohe Standards an Präzision und Qualität. Daher betont die Kultur von TCC ein Engagement für Exzellenz und die Verfolgung der Perfektion in jeder Phase des Produktionsprozesses."
+    },
+    {
+      "source": "Adaptação e Agilidade",
+      "target_en": "Adaptation and Agility",
+      "target_fr": "Adaptation et Agilité",
+      "target_es": "Adaptación y Agilidad",
+      "target_de": "Anpassungsfähigkeit und Agilität"
+    },
+    {
+      "source": "O mercado de fabricação está em constante evolução, com novas demandas e tecnologias emergentes. Portanto, a TCC adapta-se rapidamente às mudanças e responde de forma ágil às necessidades dos seus clientes.",
+      "target_en": "The manufacturing market is constantly evolving, with new demands and emerging technologies. Therefore, TCC adapts quickly to changes and responds agilely to the needs of its customers.",
+      "target_fr": "Le marché de la fabrication évolue constamment, avec de nouvelles demandes et technologies émergentes. Par conséquent, TCC s'adapte rapidement aux changements et répond de manière agile aux besoins de ses clients.",
+      "target_es": "El mercado de fabricación está en constante evolución, con nuevas demandas y tecnologías emergentes. Por lo tanto, TCC se adapta rápidamente a los cambios y responde ágilmente a las necesidades de sus clientes.",
+      "target_de": "Der Fertigungsmarkt entwickelt sich ständig weiter, mit neuen Anforderungen und aufkommenden Technologien. Daher passt sich TCC schnell an Veränderungen an und reagiert agil auf die Bedürfnisse seiner Kunden."
+    },
+    {
+      "source": "Desenvolvimento profissional",
+      "target_en": "Professional Development",
+      "target_fr": "Développement professionnel",
+      "target_es": "Desarrollo profesional",
+      "target_de": "Berufliche Entwicklung"
+    },
+    {
+      "source": "A TCC investe no desenvolvimento e crescimento profissional dos seus funcionários, oferece oportunidades de aprendizagem e desenvolvimento de habilidades para manter a sua equipa atualizada com as mais recentes tecnologias e práticas da indústria.",
+      "target_en": "TCC invests in the professional development and growth of its employees, providing learning opportunities and skill development to keep its team updated with the latest industry technologies and practices.",
+      "target_fr": "TCC investit dans le développement professionnel et la croissance de ses employés, offrant des opportunités d'apprentissage et de développement des compétences pour maintenir son équipe à jour avec les dernières technologies et pratiques de l'industrie.",
+      "target_es": "TCC invierte en el desarrollo y crecimiento profesional de sus empleados, proporcionando oportunidades de aprendizaje y desarrollo de habilidades para mantener a su equipo actualizado con las últimas tecnologías y prácticas de la industria.",
+      "target_de": "TCC investiert in die berufliche Entwicklung und das Wachstum seiner Mitarbeiter, indem es Lernmöglichkeiten und die Entwicklung von Fähigkeiten bietet, um sein Team mit den neuesten Technologien und Praktiken der Branche auf dem Laufenden zu halten."
+    },
+
   ],
 };
